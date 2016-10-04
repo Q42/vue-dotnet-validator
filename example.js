e2a22092c215424723531950e16b256cff304fc9
Ted de Koning <ted@q42.nl>
2016-10-04 16:34:15 +0200

    Expanding the example to include a custom validator.

--- example.js
+++ example.js
@@ -2,18 +2,32 @@ import dotnetValidator from './dist/index';
 
 var customValidators = {};
 
-class CustomValidator extends dotnetValidator.BaseValidator {
+class MyCustomValidator extends dotnetValidator.BaseValidator {
     isValid(value) {
         return !value || value == 'Hello';
     }
 }
 
-customValidators.Customvalidator = CustomValidator;
+customValidators.Mycustomvalidator = MyCustomValidator;
 
 
 
 Vue.component('validator', dotnetValidator.validator(customValidators));
 Vue.component('vue-dotnet-validator-group', dotnetValidator.validatorGroup);
+
+
+Vue.component('test-component', {
+  data() {
+    return {
+        inputValue: ''
+    }
+  },
+    watch: {
+      inputValue() {
+          console.log('watcher, ', this.inputValue);
+      }
+    }
+});
 
 new Vue({
     el: '#example-site'
