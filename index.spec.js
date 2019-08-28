0bd942c36a84e2276fb26e044030ef38ee1ab580
Guido <guido@q42.nl>
2019-08-28 15:59:58 +0200

    split tests & homogenize test naming

--- index.spec.js
+++ index.spec.js
@@ -2,11 +2,13 @@ import { validatorGroup, validator, BaseValidator } from './dist/index';
 
 describe('Load module', () => {
 
-  it('Allow ESM import statement', () => {
+  it('Should allow ESM import statement', () => {
     expect(validatorGroup).toBeDefined();
     expect(validator).toBeDefined();
     expect(BaseValidator).toBeDefined();
+  });
 
+  it('Should load Vue components', () => {
     expect(validatorGroup).toHaveProperty('name', 'vue-dotnet-validator-group');
     const validatorInstance = validator();
     expect(validatorInstance).toHaveProperty('name', 'vue-dotnet-validator');
